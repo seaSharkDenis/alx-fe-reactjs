a8465259775a39779f3078eb73979b774ca9e573
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-12 06:43:26 +0300

    Correctly renamed the footer component to Footer instead of footer

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-function footer(){
+function Footer(){
     return(
         <footer>
             <p>&copy; 2023 City Lovers</p>
@@ -6,4 +6,4 @@ function footer(){
     );
 }
 
-export default footer;
+export default Footer;
