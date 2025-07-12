730d0de4c955dd1dfcda0d331d39010af9171209
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-12 06:52:37 +0300

    Updated the UserProfile component to have additional text on the paragraph tags; Age: and Bio:

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -2,8 +2,8 @@ const UserProfile = (props) => {
   return (
     <div>
       <h2>{props.name}</h2>
-      <p>{props.age}</p>
-      <p>{props.bio}</p>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 };
