a3c7004af75aae43d8ba611c042feac444e3f32c
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-12 06:34:27 +0300

    Added the following containers; Footer, Header, and MainComponent. Exported them and imported them in the App.jsx. Used the components in the return part of our App function

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainComponent";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
 
   return (
     <>
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
     </>
   );
 }
---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function footer(){
+    return(
+        <footer>
+            <p>@ 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default footer;
