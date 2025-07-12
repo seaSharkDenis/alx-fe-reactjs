a5f1ea7cdcfc9fa2640b489ed64413df269be729
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-12 06:38:27 +0300

    Included a missed fullstop in the the main paragraph

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent(){
     return(
         <div>
             <main>
-                <p>I love to visit New York, Paris, and Tokyo</p>
+                <p>I love to visit New York, Paris, and Tokyo.</p>
             </main>
         </div>
     );
