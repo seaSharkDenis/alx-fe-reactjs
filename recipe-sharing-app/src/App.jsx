7f3653c7017167b5496d486d5b9a3589f81184a2
Denis Ndiritu <denisndiritu1@gmail.com>
2025-07-25 18:24:45 +0300

    Setup project directory and initial setup files, configured Zustand store and provided source code for RecipeList and AddRecipeform components.

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,17 @@
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+function App() {
+  return (
+    <>
+    <div>
+      <RecipeList />
+    </div>
+    <div>
+      <AddRecipeForm />
+    </div>
+    </>
+  )
+}
+
+export default App;
