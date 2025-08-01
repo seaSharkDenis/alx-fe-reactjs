f25321cf8bea43cb176acba98ab46637600f3e78
Denis Ndiritu <denisndiritu1@gmail.com>
2025-08-01 16:28:34 +0300

    Implemented axios on the fetchUserData file

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,17 @@
 // import.meta.env.VITE_APP_GITHUB_API_KEY;
+import axios from "axios";
 
 async function fetchUserData(username) {
-  const response = await fetch(`https://api.github.com/users/${username}`);
-  if (!response.ok) {
-    throw new Error("User not found");
-  }
-  return await response.json();
+    try{
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        return response.data;
+    }catch(error){
+        if(error.response && error.response.status === 404){
+            throw new Error("User not found");
+        }else{
+            throw new Error("Something went wrong");
+        }
+    }
 }
 
 export default fetchUserData;
