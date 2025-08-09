ed54ad9d61e8c16944c1fb5d71f53a7fbaa2122f
Denis Ndiritu <denisndiritu1@gmail.com>
2025-08-09 10:58:46 +0300

    Added a hover efect to the image to grow slightly on hover, and added a transition utility to make the scale change smooth. Included a hover effect to apply on text upon hovering. Enhanced a shadows when the card is hovered upon

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 function UserProfile() {
   return (
-    <div className="mx-auto my-20 max-w-xs bg-gray-100 rounded-lg shadow-lg p-4 sm:max-w-sm sm:p-4 md:p-8 md:max-w-sm">
-      <img className="mx-auto rounded-full w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36" src="https://img.icons8.com/?size=100&id=108639&format=png&color=000000" alt="User" />
-      <h1 className="mt-4 text-lg font-medium md:text-xl sm:text-xl text-blue-800">John Doe</h1>
+    <div className="mx-auto my-20 max-w-xs bg-gray-100 rounded-lg shadow-lg p-4 sm:max-w-sm sm:p-4 md:p-8 md:max-w-sm hover:shadow-xl">
+      <img className="mx-auto rounded-full w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out" src="https://img.icons8.com/?size=100&id=108639&format=png&color=000000" alt="User" />
+      <h1 className="mt-4 text-lg font-medium md:text-xl sm:text-xl text-blue-800 hover:text-blue-500">John Doe</h1>
       <p className="mt-2 text-sm text-gray-600 sm:text-base md:text-lg">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
